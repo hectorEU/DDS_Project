f23a721c62def8602a2b30446c3b78f9eed4a8df
Joar Andreas Gjersund <joargjersund@gmail.com>
2018-11-22 14:01:14 +0100

    even faster..

--- rsa_accelerator/hdl/montgomery.vhd
+++ rsa_accelerator/hdl/montgomery.vhd
@@ -31,20 +31,23 @@ end montgomery;
 
 architecture montgomery of montgomery is
 
-
 signal Shreg    : std_logic_vector(MONT_BLOCK_SIZE-1 downto 0);
 signal counter    : std_logic_vector(7 downto 0);
-signal r_current,r_last,r_temp    : std_logic_vector(MONT_BLOCK_SIZE-1 downto 0);
+signal r_current,r_last    : std_logic_vector(MONT_BLOCK_SIZE-1 downto 0);
 
 TYPE State_type IS (INIT, ONE, TWO, THREE, FOUR, FIVE, READY_RETURN, READY_READ); -- the 4 different states
 SIGNAL State,State_next: State_Type;   -- Create a signal that uses 
 	
 signal shifter_lr, msbfound :natural;
+
 begin
 
 
+process (clk, reset_n) 
+variable r_temp, r_temp2    : std_logic_vector(MONT_BLOCK_SIZE-1 downto 0);
+begin
 
-process (clk, reset_n) begin
+
 if (reset_n = '0') then
 State <= INIT;
 elsif ( (rising_edge(clk)) and reset_n='1') then
@@ -53,56 +56,40 @@ elsif ( (rising_edge(clk)) and reset_n='1') then
             msbfound<=0;
             shifter_lr <= 0;
             ready_out <='0';
-            counter <= std_logic_vector(to_unsigned(0,8));
+            counter <= std_logic_vector(to_unsigned(1,8));
             Shreg <= a; -- load a.
             r_current <= std_logic_vector(to_unsigned(0,MONT_BLOCK_SIZE)); -- r = 0;
-            r_last <= std_logic_vector(to_unsigned(0,MONT_BLOCK_SIZE)); -- r = 0;
-            r_temp <= std_logic_vector(to_unsigned(0,MONT_BLOCK_SIZE)); -- r = 0;
             if (ready_in = '1') then 
-            State_next <= ONE;
-            end if;
-        WHEN ONE =>
-            ready_out <='0';
-            if (Shreg(shifter_lr) = '1') then -- ((a >> i) & 1) * b;
-                r_current <= r_last + b;
-            else
-                r_current <= r_last;
-            end if;
-
-            
-            counter <= counter + 1;
-            shifter_lr <= shifter_lr +1;
-            
-        --    Shreg <= '0' & Shreg(MONT_BLOCK_SIZE-1 downto 1);     -- (a >> i
-            
             State_next <= TWO;
+            end if;
        WHEN TWO =>
             ready_out <='0';
-            if(r_last(0) = '0') then
-                r_current <= '0' & r_last(MONT_BLOCK_SIZE-1 downto 1);     -- r = r >> 1;
-                State_next <= FOUR;
-            else
-                r_temp <= r_last + key_n;
-                r_current <= r_last;
-                State_next <= THREE;
-            end if;
-      WHEN THREE =>
-            ready_out <='0';
-            r_current <=  '0' & r_temp(MONT_BLOCK_SIZE-1 downto 1); 
-            if (counter = 0) then
-                State_next <= READY_RETURN;
-            else
-                State_next <= ONE;
-            end if;
             
-     WHEN FOUR =>
-            ready_out <='0';
-            r_current <= r_last;
-            if (counter = 0) then
-                State_next <= READY_RETURN;
+            if (Shreg(shifter_lr) = '1') then -- ((a >> i) & 1) * b;
+                r_temp2 := r_last + b;
             else
-                State_next <= ONE;
+                r_temp2 := r_last;
             end if;
+                      
+            counter <= counter + 1;
+            shifter_lr <= shifter_lr +1;            
+            
+            
+            if(r_temp2(0) = '0') then
+              --  r_current <= r_last;
+                r_temp := r_temp2;
+            else
+               
+                r_temp := r_temp2 + key_n;
+                
+            end if;
+             r_current <=  '0' & r_temp(MONT_BLOCK_SIZE-1 downto 1);
+             
+            if (counter = 0) then
+                 State_next <= READY_RETURN;
+             else
+                 State_next <= TWO;
+             end if;
 
     WHEN READY_RETURN =>
             ready_out <='1';
